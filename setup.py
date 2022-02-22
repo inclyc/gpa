d86702686af9a7d9fc0cbdc6cb2906a8445b74d1
inclyc <axoford@icloud.com>
2022-02-22 21:02:46 +0800

    Bump version 0.1.4, fix bug

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='gpalib',
-    version='0.1.3',
+    version='0.1.4',
     description='A library to convert scores using many arithmetics',
     long_description=long_description,
     long_description_content_type='text/markdown',
---
+++ test.py
@@ -0,0 +1,4 @@
+import gpalib
+a = [99, 87, 89, 83, 96]
+r = gpalib.convert_to_4(a, 'hundred', 'Beida_4')
+print(r)
